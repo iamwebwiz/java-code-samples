55adf210428ebc13267bb90719bb1977682b1f0d
iamwebwiz <iamwebwiz@gmail.com>
2017-08-14 04:11:53 +0100

    changes made

---
+++ Adeyemi College of Education/Maths Dept/Group 1/Group1b.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Group1b {
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		FileWriter fw = new FileWriter("Group1bOutput.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		StringBuilder builder = new StringBuilder();
+
+		double[][] P = new double[5][4];
+		double[][] R = new double[5][4];
+		double[][] T = new double[5][4];
+		double[][] SI = new double[5][4];
+
+		int count = 1;
+		// Enter values for P, R and T to calculate the SI unit
+		System.out.println("Enter values for P, R and T");
+		for (int row = 0; row < SI.length; row++){
+			for (int col = 0; col < SI[row].length; col++){
+				System.out.print("P" + count + " = ");
+				P[row][col] = input.nextDouble();
+
+				System.out.print("R" + count + " = ");
+				R[row][col] = input.nextDouble();
+
+				System.out.print("T" + count + " = ");
+				T[row][col] = input.nextDouble();
+
+				System.out.println();
+				count++;
+			}
+		}
+
+		// calculate the simple interest on each of the values
+		count = 1;
+		for (int row = 0; row<SI.length; row++){
+			for (int col=0; col < SI[row].length; col++){
+				SI[row][col] = (P[row][col] * R[row][col] * T[row][col]) / 100;
+				// display the values of simple interest
+				System.out.println("SI_" + count + " = " + SI[row][col]);
+				count++;
+			}
+			System.out.println();
+		}
+
+		count = 1;
+		// write data to file
+		try {
+			for (int row = 0; row < SI.length; row++){
+				for (int col = 0; col < SI[row].length; col++){
+					builder.append("SI_" + count + " = " + SI[row][col]);
+					count++;
+					if (col < SI.length - 1){
+						builder.append("\n");
+					}
+				}
+				builder.append("\n");
+			}
+			bw.write(builder.toString());
+			bw.close();
+		}
+		catch (IOException err){
+			System.out.println("Could not write data to file...");
+			err.printStackTrace();
+		}
+		catch (Exception err){
+			System.out.println("An error occured");
+			err.printStackTrace();
+		}
+	}
+}
---
+++ Adeyemi College of Education/Maths Dept/Group 1/Group1c.java
@@ -0,0 +1,13 @@
+// V = 1/3 * Math.PI * Math.pow(r, 2) * h
+
+import java.io.*;
+import java.util.*;
+
+public class Group1c {
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		FileWriter fw = new FileWriter("Group1cOutput.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		StringBuilder builder = new StringBuilder();
+	}
+}
