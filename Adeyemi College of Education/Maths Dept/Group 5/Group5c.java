64333d7f8527830a3f30df4a2b75765116d6e54f
iamwebwiz <iamwebwiz@gmail.com>
2017-08-16 03:18:04 +0100

    files added

---
+++ Adeyemi College of Education/Maths Dept/Group 5/Group5c.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Group5c {
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		StringBuilder builder = new StringBuilder();
+	}
+}
