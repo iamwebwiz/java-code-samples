74f2c95c3d04ec145a654ca05b6565337aed2730
iamwebwiz <iamwebwiz@gmail.com>
2017-11-22 00:55:42 +0100

    Booking class done from UML

---
+++ Booking System/Booking.java
@@ -0,0 +1,21 @@
+class Booking
+{
+    private String room;
+    private String name;
+
+    public Booking(String room, String name)
+    {
+        this.room = room;
+        this.name = name;
+    }
+
+    public String getRoom()
+    {
+        return this.room;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+}
