e516245aeec16571f23ef2924b6b0e4d31ae3662
iamwebwiz <iamwebwiz@gmail.com>
2017-11-22 02:57:05 +0100

    TimeTable class filled with some data

---
+++ Booking System/TimeTable.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class TimeTable
+{
+    private Booking[][] times;
+
+    public TimeTable(int days, int periods)
+    {
+        this.times = new Booking[days][periods];
+    }
+
+    public boolean makeBooking(int day, int period, Booking booking)
+    {
+        //
+    }
+
+    public boolean isFree(int day, int period)
+    {
+        //
+    }
+
+    public Booking getBooking(int day, int period)
+    {
+        //
+    }
+
+    public int numberOfDays()
+    {
+        //
+    }
+
+    public int numberOfPeriods()
+    {
+        //
+    }
+}
